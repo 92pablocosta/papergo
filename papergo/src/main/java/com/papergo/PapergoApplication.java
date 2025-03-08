6de69d8989f7e82b464e1b6fd4309cbb803627af
Pablo Costa <164939584+92pablocosta@users.noreply.github.com>
2025-03-07 23:40:47 -0300

    Cria o springboot/maven para o projeto. inclui API

---
+++ papergo/src/main/java/com/papergo/PapergoApplication.java
@@ -0,0 +1,13 @@
+package com.papergo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapergoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapergoApplication.class, args);
+	}
+
+}
