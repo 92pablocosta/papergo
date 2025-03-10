45df754ae560533fcc9805c0766dc298ad2fdf19
Pablo Costa <164939584+92pablocosta@users.noreply.github.com>
2025-03-10 12:48:00 -0300

    arquivo ruby

--- main.rb
+++ main.rb
@@ -2,12 +2,21 @@ puts "Hello, world!"
 
 name = "Pablo"
 idade = 32
-puts "Meu nome é #{nome} e eu tenho #{idade} anos"
+puts "Meu nome é #{name} e eu tenho #{idade} anos"
 
-{"nome" => "Pablo", "idade" => 32}
+{"name" => "Pablo", "idade" => 32}
 
 if idade >= 18
     puts "Você é maior de idade"
 else
     puts "Você é menor de idade"
 end
+
+# comentário de uma linha
+
+=begin
+  comentário de múltiplas linhas
+  pode ocupar várias linhas de boas
+  tudo aqui vai ser ignorado, fala que pablo vittar é homem que tá tudo bem
+=end
+nome = "Caio"
