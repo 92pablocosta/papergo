379963129fc6051e7274920512cd29852b29a80f
Pablo Costa <164939584+92pablocosta@users.noreply.github.com>
2025-03-10 12:40:44 -0300

    imagem conceita + json

---
+++ main.rb
@@ -0,0 +1,13 @@
+puts "Hello, world!"
+
+name = "Pablo"
+idade = 32
+puts "Meu nome é #{nome} e eu tenho #{idade} anos"
+
+{"nome" => "Pablo", "idade" => 32}
+
+if idade >= 18
+    puts "Você é maior de idade"
+else
+    puts "Você é menor de idade"
+end
